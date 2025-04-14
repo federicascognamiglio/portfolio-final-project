49244834a927a04f4cfc9fb513b82afc166043f6
federicascognamiglio <scognamigliofede99@gmail.com>
2025-04-14 17:36:34 +0200

    feat: categories table seeder created

---
+++ database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\CategoryEnum;
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach (CategoryEnum::cases() as $case) {
+            Category::updateOrCreate(
+                ['name' => $case->value],
+            );
+        }
+    }
+}
