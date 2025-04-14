6622cc6a3daead838e305d951aa0b8df8672d2ab
federicascognamiglio <scognamigliofede99@gmail.com>
2025-04-14 19:37:40 +0200

    feat: type Enum created

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'category_id'];
     
     public function categories()
     {
---
+++ app/TypeEnum.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+enum TypeEnum: string
+{
+    case EDITORIAL = 'Editorial';
+    case BRAND_IDENTITY = 'Brand Identity';
+    case PACKAGING = 'Packaging';
+    case INSTAGRAM = 'Instagram';
+    case TIK_TOK = 'TikTok';
+    case YOUTUBE = 'YouTube';
+    case LINKEDIN = 'LinkedIn';
+    case PRODUCT = 'Product';
+    case ANALOGICAL = 'Analogical';
+    case CONCEPTUAL = 'Conceptual';
+    case THREE_D_MODELING = '3D Modeling';
+    case THREE_D_ANIMATION = '3D Animation';
+    case TWO_D_ANIMATION = '2D Animation';
+    case INFOGRAPHICS = 'Infographics';
+    case GIFS = 'GIFs';
+
+
+
+    public function categoryId(): int
+    {
+        return match ($this) {
+            // Categoria: "Graphic Design"
+            self::EDITORIAL, self::BRAND_IDENTITY, self::PACKAGING  => 1,
+            // Categoria: "Social Media"
+            self::INSTAGRAM, self::TIK_TOK, self::YOUTUBE, self::LINKEDIN => 2,
+            // Categoria: "Photography"
+            self::PRODUCT, self::ANALOGICAL, self::CONCEPTUAL => 3,
+            // Categoria: "3D"
+            self::THREE_D_MODELING, self::THREE_D_ANIMATION => 4,
+            // Categoria: "Motion Graphics"
+            self::TWO_D_ANIMATION, self::INFOGRAPHICS, self::GIFS => 5,
+        };
+    }
+}
