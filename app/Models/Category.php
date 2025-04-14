281e2645c9cafb54887424255f41310ca9c8189f
federicascognamiglio <scognamigliofede99@gmail.com>
2025-04-14 19:38:03 +0200

    feat: category model updated

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name', 'type_id'];
+    protected $fillable = ['name', 'description'];
     
     public function projects()
     {
