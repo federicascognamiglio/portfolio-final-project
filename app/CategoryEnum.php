9d09806c5dd97bf903276a330e03624f34f3bc96
federicascognamiglio <scognamigliofede99@gmail.com>
2025-04-14 17:36:02 +0200

    feat: category Enum created

---
+++ app/CategoryEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+enum CategoryEnum: string
+{
+    case GRAPHIC_DESIGN = 'Graphic Design';
+    case THREE_D = '3D';
+    case MOTION_GRAPHICS = 'Motion Graphics';
+    case SOCIAL_MEDIA = 'Social Media';
+    case PHOTOGRAPHY = 'Photography';
+}
