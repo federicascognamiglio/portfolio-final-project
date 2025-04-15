fe9c25be7266da97c37cbc93720bcd51f8375b47
federicascognamiglio <scognamigliofede99@gmail.com>
2025-04-15 13:39:38 +0200

    feat: project controller and route created

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Project;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Get status from the request query parameters if exists
+        $status = request()->query('status');
+
+        // Fetch projects based on the status filter
+        if ($status) {
+            $projects = Project::where('status', $status)->get();
+        } else {
+            $projects = Project::all();
+        }
+        
+        return view('projects.index', compact('projects', 'status'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
